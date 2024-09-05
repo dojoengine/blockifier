6505c115f90f296ea9f78769aa33c07916e5a019
Ammar Arif <evergreenkary@gmail.com>
2024-09-05 16:04:26 -0400

    style: clippy

--- scripts/clippy.sh
+++ scripts/clippy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cargo clippy "$@" --all-targets --all-features -- -D warnings -D future-incompatible \
+cargo +nightly-2024-01-12 clippy "$@" --all-targets --all-features -- -D warnings -D future-incompatible \
     -D nonstandard-style -D rust-2018-idioms -D unused
